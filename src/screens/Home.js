ae4f6e3478b2e2a45c11da756c08dd56fe262f36
Hamza Altaf <86216890+hamza2227@users.noreply.github.com>
2022-12-26 16:29:53 +0500

    Profile Choose Avatar

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Link, Carousel, Button, InputGroup, Accordion, FormControl, Row, Col, ProgressBar, Form, Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+// import Sonnet from '../../components/Sonnet';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Offcanvas from 'react-bootstrap/Offcanvas';
@@ -12,14 +15,189 @@ export default function Index() {
         <div>
 
 
-            {/* .... Persona Sidebar......... */}
-
-            <div className='forgot_psw_page'>
+            {/* .... Persona profile Sidebar......... */}
+            <div className='_profile'>
                 <Example className='next_btn' />
             </div>
-
             {/* End.... */}
 
+            {/* .....Profile Fill detail */}
+            <div className='pro_fill_detail'>
+                <div className='_close_window'>
+                    <i class="fa-regular fa-xmark"></i>
+                </div>
+
+                <div className='fill_detail_container'>
+                    <div className='_user_profile'>
+                        <img src={require('../images/puppyAvatar.png')} style={{ width: "170px" }} />
+                        <p>Tim Jacob</p>
+                    </div>
+                    <div className='_user_stacks'>
+                        <div className='user_day_streaks'>
+                            <img src={require('../images/flame.png')} style={{ width: "48px" }} />
+                            <p>09<span>Day streak</span></p>
+                        </div>
+                        <div className='user_day_streaks'>
+                            <img src={require('../images/thunderbolt.png')} style={{ width: "48px" }} />
+                            <p>124<span>XP</span></p>
+                        </div>
+                        <div className='user_day_streaks'>
+                            <img src={require('../images/medal.png')} style={{ width: "48px" }} />
+                            <p>264<span>Badges</span></p>
+                        </div>
+                    </div>
+                    <div className='_user_stacks'>
+                        <div className='user_day_streaks'>
+                            <img src={require('../images/trophy.png')} style={{ width: "48px" }} />
+                            <p>02<span>Trophies</span></p>
+                        </div>
+                        <div className='user_day_streaks'>
+                            <img src={require('../images/certificate.jpg')} style={{ width: "48px" }} />
+                            <p>02<span>Certificates</span></p>
+                        </div>
+                    </div>
+
+                    <button className='change_profile_btn'>Change Profile Picture</button>
+                </div>
+            </div>
+            {/* End.... */}
+
+            {/* .....Profile Choose Avatar */}
+            <div className='pro_choose_avatar'>
+                <div className='_close_window'>
+                    <i class="fa-regular fa-xmark"></i>
+                </div>
+
+                <div className='fill_detail_container'>
+                    <div className='_user_profile'>
+                        <img src={require('../images/puppyAvatar.png')} style={{ width: "85px" }} />
+                        <div className='auhtor_name'>
+                            <h2>Tim Jacob</h2>
+                            <p>Please choose your Avatar</p>
+                        </div>
+                    </div>
+
+                    <div className='avatar_tabs'>
+                        <Tabs
+                            defaultActiveKey="profile"
+                            id="uncontrolled-tab-example"
+                            className="mb-3"
+                        >
+                            <Tab eventKey="home" title="Home">
+
+                                <div className='_avatars_collection'>
+                                    <img src={require('../images/puppyAvatar.png')} style={{ width: "85px" }} />
+                                    <img src={require('../images/puppyAvatar.png')} style={{ width: "85px" }} />
+                                    <img src={require('../images/puppyAvatar.png')} style={{ width: "85px" }} />
+                                    <img src={require('../images/puppyAvatar.png')} style={{ width: "85px" }} />
+                                    <img src={require('../images/puppyAvatar.png')} style={{ width: "85px" }} />
+                                    <img src={require('../images/puppyAvatar.png')} style={{ width: "85px" }} />
+                                    <img src={require('../images/puppyAvatar.png')} style={{ width: "85px" }} />
+                                </div>
+                                <div className='_avatars_collection'>
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                </div>
+                                <div className='_avatars_collection'>
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/rabit.jpg')} style={{ width: "85px" }} />
+                                </div>
+                                <div className='_avatars_collection'>
+                                    <img src={require('../images/dog.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/dog.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/dog.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/dog.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/dog.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/dog.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/dog.jpg')} style={{ width: "85px" }} />
+                                </div>
+                                <div className='_avatars_collection'>
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                </div>
+                                <div className='_avatars_collection'>
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mouse.jpg')} style={{ width: "85px" }} />
+                                </div>
+
+                            </Tab>
+                            <Tab eventKey="profile" title="Profile">
+                                <div className='_patches_collection'>
+                                    <img src={require('../images/mountainB.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainB.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainB.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainB.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainB.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainB.jpg')} style={{ width: "85px" }} />
+                                </div>
+                                <div className='_patches_collection'>
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                </div>
+                                <div className='_patches_collection'>
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                </div>
+                                <div className='_patches_collection'>
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                </div>
+                                <div className='_patches_collection'>
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainO.jpg')} style={{ width: "85px" }} />
+                                </div>
+                                <div className='_patches_collection'>
+                                    <img src={require('../images/mountainB.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainB.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainB.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainB.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainB.jpg')} style={{ width: "85px" }} />
+                                    <img src={require('../images/mountainB.jpg')} style={{ width: "85px" }} />
+                                </div>
+                            </Tab>
+                        </Tabs>
+                    </div>
+
+
+                </div>
+            </div>
+            {/* End.... */}
 
         </div>
 
